126783fd71a2481b0e0a910948ff2d5b04a3000a
Rouven Spaar <rouven.spaar@gmail.com>
2025-03-07 19:07:34 +0100

    refactor

--- wyscout/main.py
+++ wyscout/main.py
@@ -8,9 +8,9 @@ files = ["5574548", "5574557", "5662871", "5637448"] # 5684335 is missing
 if __name__ == "__main__":
     load_dotenv()
 
-    json_file_path = os.getenv("JSON_FILE_PATH")
+    json_file_path = os.getenv("WYSCOUT_PATH")
     if not json_file_path:
-        raise ValueError("JSON_FILE_PATH is not set in .env file")
+        raise ValueError("WYSCOUT_PATH is not set in .env file")
 
     json_file_path += files[0]
     json_file_path += ".json"
